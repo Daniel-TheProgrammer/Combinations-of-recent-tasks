36422344028339e281d5df8858d0261f30f85557
NJI KEBEI DANIEL KOUM <60320246+Daniel-TheProgrammer@users.noreply.github.com>
2020-08-18 12:19:12 +0100

    Add files via upload

--- end.js
+++ end.js
@@ -2,11 +2,6 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-
-const MAX_HIGH_SCORES = 5;
-
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -15,15 +10,4 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = (e) => {
     e.preventDefault();
-
-    const score = {
-        score: mostRecentScore,
-        name: username.value,
-    };
-    highScores.push(score);
-    highScores.sort((a, b) => b.score - a.score);
-    highScores.splice(5);
-
-    localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
 };
